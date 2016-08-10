2721676797a68fe20f4a66717e26e89b9a9ec3ba
Albert Nguyen <Albert.la.nguyen@gmail.com>
2016-08-09 17:49:22 -0700

    implemented searchable module

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -1,8 +1,26 @@
+
 require_relative 'db_connection'
 require_relative '01_sql_object'
 
 module Searchable
+  def where(params)
+    where_line = params.keys.map do |key|
+      "#{key} = ?"
+    end.join(" AND ")
 
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    results.map do |result|
+      self.new(result)
+    end
+  end
 end
 
 class SQLObject
